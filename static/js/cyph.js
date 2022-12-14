87d6461a3d2e8e0cae7b2567015b43fa6b9266d1
cyph <239076@hononegah.org>
2022-12-14 07:54:08 -0600

    aaaa

--- static/js/cyph.js
+++ static/js/cyph.js
@@ -4,8 +4,12 @@ function translateContainerUp() {
 }
 
 function translateContainerDown() {
-  const container = document.getElementById('container');
-  container.style.transform = 'translate(-50%, -50%)';
+  closePortfolio();
+  setTimeout(couldntThinkOfAFunctionName, 100);
+  function couldntThinkOfAFunctionName() {
+    const container = document.getElementById('container');
+    container.style.transform = 'translate(-50%, -50%)';
+  }
 }
 
 function loadPortfolio() {
@@ -19,7 +23,7 @@ function loadPortfolio() {
           <li><a class="fa-brands fa-github" href="https://github.com/cyphpw" title="Github"></a></li>
           <li><a class="fa-brands fa-youtube" href="https://www.youtube.com/channel/UCRb1EafkijQzJRPV7J8KDhA" title="Youtube"></a></li>
           <li><a class="fa-brands fa-steam" href="https://steamcommunity.com/id/cy_ph/" title="Steam"></a></li>
-          <li><a class="fa-solid fa-file" title="Portfolio" onclick="translateContainerDown(); closePortfolio()"></a></li>
+          <li><a class="fa-solid fa-file" title="Portfolio" onclick="translateContainerDown();"></a></li>
         </ul>
       <div id="portfolio">
         <div id="what">
@@ -60,50 +64,16 @@ function loadPortfolio() {
 }
 
 function closePortfolio() {
-  setTimeout(actualClose, 1100);
-  function actualClose() {
     const container = document.getElementById('container');
     container.innerHTML =
-        `<h1>cyph<span>.pw</span></h1>
-        <ul>
-          <li><a class="fa-brands fa-discord" href="https://discordlookup.com/user/272440015575973889" title="Discord"></a></li>
-          <li><a class="fa-brands fa-github" href="https://github.com/cyphpw" title="Github"></a></li>
-          <li><a class="fa-brands fa-youtube" href="https://www.youtube.com/channel/UCRb1EafkijQzJRPV7J8KDhA" title="Youtube"></a></li>
-          <li><a class="fa-brands fa-steam" href="https://steamcommunity.com/id/cy_ph/" title="Steam"></a></li>
-          <li><a class="fa-solid fa-file" title="Portfolio" onclick="translateContainerUp(); loadPortfolio() portfolio.classlist.toggle('fade')"></a></li>
-        </ul>
-      <div id="portfolio">
-        <div id="what">
-          <h2><span>What</span> do you do?</h2>
-          <p>I am a Network Engineer and do Web Development as a hobby and a class at my school.</p>
-        </div>
-        <div id="why">
-            <h2><span>Why</span> do you do it?</h2>
-            <p>It's what I love to do, and I'm exploring more things as I go along in my career. Eventually I want to develop cheats for games and get into cybersecurity.</p>
-        </div>
-        <div id="where">
-            <h2><span>Where</span> can I find work you've done?</h2>
-            <div id="spanhack"><p>Right at the bottom of this page. <span>(psst.. keep scrolling)</span></p></div>
-        </div>
-      
-        <div class="box">
-              <a class="item1" href="https://cyphware.xyz"><div></div></a>
-              <a class="item2" href="https://cyph.pw"><div></div></a>
-        </div>
-          
-        <div id="cyphware">
-            <h2>cyphware:</h2>
-            <p>cyphware is a proof of concept/showcase website for a game cheat I want to make with the same name.</p>
-        </div>
-        <div id="cyphpw">
-            <h2>cyph.pw:</h2>
-            <p>cyph.pw is a short url page I can send to someone so they can easily access most of my socials.</p>
-        </div>
-      </div>
-      </div>
-      </div>`;
-      
-  }
+    `<h1>cyph<span>.pw</span></h1>
+    <ul>
+      <li><a class="fa-brands fa-discord" href="https://discordlookup.com/user/272440015575973889" title="Discord"></a></li>
+      <li><a class="fa-brands fa-github" href="https://github.com/cyphpw" title="Github"></a></li>
+      <li><a class="fa-brands fa-youtube" href="https://www.youtube.com/channel/UCRb1EafkijQzJRPV7J8KDhA" title="Youtube"></a></li>
+      <li><a class="fa-brands fa-steam" href="https://steamcommunity.com/id/cy_ph/" title="Steam"></a></li>
+      <li><a class="fa-solid fa-file" title="Portfolio" onclick="translateContainerUp(); loadPortfolio()"></a></li>
+    </ul>`;
 
 }
 
